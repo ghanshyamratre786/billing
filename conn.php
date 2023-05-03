fd705c8529db2d61d4dd1f9c524a5eab9d67f308
komallahre35 <komallahre35@gmail.com>
2023-05-03 22:41:45 +0530

    second

--- conn.php
+++ conn.php
@@ -4,17 +4,21 @@ ini_set('memory_limit', '512M');
 date_default_timezone_set("Asia/Kolkata");
 if($_SERVER["SERVER_NAME"]=="localhost" || $_SERVER["SERVER_NAME"]=="Komal"|| $_SERVER["SERVER_NAME"]=="Komal")
 {
-	$host_name="localhost";
-	$db_name="pet_diagnostics"; 
-	$db_user="root";
-	$db_pwd="";
+	// $host_name="localhost";
+	// $db_name="pet_diagnostics"; 
+	// $db_user="root";
+	// $db_pwd="";
+	$host_name="csgi-discorso.cqqqfbf6vzmy.ap-south-1.rds.amazonaws.com";
+	$db_name="billing";
+	$db_user="csgi_discorso";
+	$db_pwd="awscsgi123";
 }
 else
 {
-	$host_name="localhost";
-	$db_name="u786450031_test";
-	$db_user="u786450031_test";
-	$db_pwd="w4K[]tRc";
+	$host_name="csgi-discorso.cqqqfbf6vzmy.ap-south-1.rds.amazonaws.com";
+	$db_name="billing";
+	$db_user="csgi_discorso";
+	$db_pwd="awscsgi123";
 	
 }
 $conn = mysqli_connect("$host_name","$db_user","$db_pwd",$db_name);
--- login/inc/menu.php
+++ login/inc/menu.php
@@ -6,30 +6,7 @@
          <span>Dashboard</span>
          </a>
       </li>
-      <li class="nav-item">
-         <a class="nav-link " href="gallary">
-         <i class="bi bi-grid"></i>
-         <span>Gallary</span>
-         </a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link " href="vet">
-         <i class="bi bi-grid"></i>
-         <span>Veterinarian</span>
-         </a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link " href="services">
-         <i class="bi bi-grid"></i>
-         <span>Services</span>
-         </a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link " href="contact">
-         <i class="bi bi-grid"></i>
-         <span>Contact</span>
-         </a>
-      </li>
+     
    </ul>
 </aside>
 <!-- End Sidebar-->
